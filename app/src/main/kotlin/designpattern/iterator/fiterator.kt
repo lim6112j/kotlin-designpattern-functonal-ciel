5dbdba00b83b9fea2aad44165cc6dbed8c8e5547
ben lim <lim6112j@gmail.com>
2023-03-14 04:22:51 +0900

    initial commit

---
+++ app/src/main/kotlin/designpattern/iterator/fiterator.kt
@@ -0,0 +1,15 @@
+package designpattern.iterator
+// type
+data class FFriend(val name: String, val age: Int)
+class FFriends(val friends: List<FFriend>, var currentIdx: Int = 0)
+// pure function
+fun FFriends.hasMore(): Boolean = this.friends.size > currentIdx
+fun FFriends.next(): FFriend? {
+  if(this.hasMore()) {
+    val currentFriend = this.friends[this.currentIdx]
+    this.currentIdx++
+    return currentFriend
+  }
+  return null;
+}
+
---
+++ app/src/main/kotlin/designpattern/iterator/iterator.kt
@@ -0,0 +1,31 @@
+package designpattern.iterator
+interface MyIterator {
+  fun hasMore() : Boolean
+  fun next() : Friend?
+}
+interface Friends {
+  val size: Int
+  fun createFriendsIterator() : MyIterator
+}
+class Friend(val name: String, val age: Int) 
+class FriendsIterator(val friends: MyFriends) : MyIterator {
+  private var currentPosition = 0
+  override fun hasMore(): Boolean  {
+    return friends.size > currentPosition
+  }
+  override fun next() : Friend? {
+    if(hasMore()) {
+      val friend =  friends.friends[currentPosition]
+      currentPosition++
+      return friend
+    }
+    return null
+  } 
+}
+class MyFriends(val friends: List<Friend>,) : Friends {
+  override val size = friends.size 
+  override fun createFriendsIterator(): FriendsIterator {
+    val iterator = FriendsIterator(this)
+    return iterator
+  }
+}
